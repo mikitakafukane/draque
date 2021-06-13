474125673197e1ccc54846c0be1b6946c040caf0
mikitakafukane <arsene.miyaniii.4@gmail.com>
2021-06-14 01:04:00 +0900

    finish 4-6

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ require './games_controller'
 
 games_controller = GamesController.new
 
-brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
-monster = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
+terry = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
+slime = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
 
 games_controller.battle(brave: terry, monster: slime)
