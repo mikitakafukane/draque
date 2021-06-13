9a9923e926cc71531f6b3bac0635bc6e71ad3212
mikitakafukane <arsene.miyaniii.4@gmail.com>
2021-06-13 19:21:15 +0900

    finish 3-3

--- main.rb
+++ main.rb
@@ -39,31 +39,55 @@ end
 
 class Monster
 
-  attr_reader :name, :offense, :defense
+  attr_reader :offense, :defense
+  attr_accessor :hp, :name
 
-  attr_accessor :hp
+  POWER_UP_RATE = 1.5
+  CALC_HALF_HP = 0.5
 
   def initialize(**params)
     @name = params[:name]
     @hp = params[:hp]
     @offense = params[:offense]
     @defense = params[:defense]
+
+    @transform_flag = false
+    @trigger_of_transform = params[:hp] * CALC_HALF_HP
+  end
+
+  def attack(brave)
+    
+    if hp < @trigger_of_transform && @transform_flag == false
+      @transform_flag = true
+      transform
+    end
+    
+    puts "#{name}の攻撃"
+
+    damage = @offense - brave.defense
+    brave.hp -= damage
+
+    puts "#{brave.name}は#{damage}のダメージを受けた"
+    puts "#{brave.name}の残りHPは#{brave.hp}だ"
+  end
+
+  private
+
+  def transform
+    transform_name = "ドラゴン"
+
+    puts <<~EOS
+    #{@name}は怒っている
+    #{@name}は#{@transform_name}に変身した
+    EOS
+
+    monster.hp * POWER_UP_RATE
+    @name = transform_name
   end
 
 end
+  brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
+  monster = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
 
-brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
-monster = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
-
-brave.attack(monster)
-
-# puts <<~TEXT
-# NAME:#{brave.name}
-# HP:#{brave.hp}
-# OFFENSE:#{brave.offense}
-# DEFENSE:#{brave.defense}
-# TEXT
-
-# brave.hp -= 30
-
-# puts "#{brave.name}はダメージを受けた！残りHPは#{brave.hp}だ"
+  brave.attack(monster)
+  monster.attack(brave)
