1a4640252ff6b4662cb12d490b73ae4e5980c193
mikitakafukane <arsene.miyaniii.4@gmail.com>
2021-06-13 18:47:23 +0900

    finish 3-1

--- main.rb
+++ main.rb
@@ -1,7 +1,6 @@
 class Brave
 
   attr_reader :name, :offense, :defense
-
   attr_accessor :hp
 
   def initialize(**params)
@@ -9,6 +8,15 @@ class Brave
     @hp = params[:hp]
     @offense = params[:offense]
     @defense = params[:defense]
+  end
+
+  def attack(monster)
+    damage = @offense - monster.defense
+    monster.hp -= damage
+
+    puts "#{@name}の攻撃"
+    puts "#{monster.name}は#{damage}のダメージを受けた"
+    puts "#{monster.name}の残りHPは#{monster.hp}だ"
   end
 
 end
@@ -31,13 +39,15 @@ end
 brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
 monster = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
 
-puts <<~TEXT
-NAME:#{brave.name}
-HP:#{brave.hp}
-OFFENSE:#{brave.offense}
-DEFENSE:#{brave.defense}
-TEXT
+brave.attack(monster)
 
-brave.hp -= 30
+# puts <<~TEXT
+# NAME:#{brave.name}
+# HP:#{brave.hp}
+# OFFENSE:#{brave.offense}
+# DEFENSE:#{brave.defense}
+# TEXT
 
-puts "#{brave.name}はダメージを受けた！残りHPは#{brave.hp}だ"
+# brave.hp -= 30
+
+# puts "#{brave.name}はダメージを受けた！残りHPは#{brave.hp}だ"
